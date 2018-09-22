33120a2699e29a0a1d3214de891bb43d364750db
Jonny Kalambay <jonnyk20@gmail.com>
2018-09-22 11:36:58 -0700

    Testing POST Request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import { Admin, Resource } from 'react-admin'
-import postgrestProvider from 'aor-postgrest-client'
-import createAuthClient from './authClient'
-import { TodosList } from './todos';
-import httpClient from "./httpClient";
+import React from 'react';
+import { Admin, Resource } from 'react-admin';
+import postgrestProvider from 'aor-postgrest-client';
+
+import createAuthClient from './authClient';
+import { TodosList, TodoCreate } from './todos';
+import httpClient from './httpClient';
 
 const dataProvider = postgrestProvider('/api', httpClient);
 const authProvider = createAuthClient('/api');
-const App = () => <Admin
-    dataProvider={dataProvider}
-    authProvider={authProvider}
->
-    <Resource name="todos" list={TodosList} />
-</Admin>;
+const App = () => (
+  <Admin dataProvider={dataProvider} authProvider={authProvider}>
+    <Resource name="todos" list={TodosList} create={TodoCreate} />
+  </Admin>
+);
 
 export default App;
--- frontend/src/todos.js
+++ frontend/src/todos.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import { List, Datagrid, TextField, EditButton } from 'react-admin';
+import {
+  Filter,
+  List,
+  Edit,
+  Create,
+  Datagrid,
+  ReferenceField,
+  TextField,
+  EditButton,
+  DisabledInput,
+  LongTextInput,
+  ReferenceInput,
+  SelectInput,
+  SimpleForm,
+  TextInput
+} from 'react-admin';
 
-export const TodosList = (props) => (
-    <List {...props}>
-        <Datagrid>
-            <TextField source="id" />
-            <TextField source="task" />
-            <EditButton basePath="/todos" />
-        </Datagrid>
-    </List>
+export const TodosList = props => (
+  <List {...props}>
+    <Datagrid>
+      <TextField source="id" />
+      <TextField source="task" />
+      <EditButton basePath="/todos" />
+    </Datagrid>
+  </List>
+);
+
+export const TodoCreate = props => (
+  <Create {...props}>
+    <SimpleForm>
+      <LongTextInput source="task" />
+    </SimpleForm>
+  </Create>
 );
