ad04bb4f181fa8d698f0d2e43f8209ee7c94e951
Saayaman <ayakosayama1987@gmail.com>
2018-09-23 11:44:28 -0700

    updage login bgimage

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,5 @@
 export const bgImages = [
-  "https://images.unsplash.com/photo-1511184117514-74b2b39697a4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=322aa46a81681ce74115bea006e0f9ac&auto=format&fit=crop&w=1650&q=80",
+  "https://images.unsplash.com/photo-1527599296290-e847cebb1d00?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7490ca4acb7e03eabe641bfe2cb028dc&auto=format&fit=crop&w=1650&q=80",
   "https://images.unsplash.com/photo-1530639834082-05bafb67fbbe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=566a60cdbd8fba6a857fb6ba585dfbaa&auto=format&fit=crop&w=1650&q=80",
   "https://images.unsplash.com/photo-1529951087676-7daba061d518?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1a6956d259b6f6c8367da1616b89cfd9&auto=format&fit=crop&w=2850&q=80",
-  "https://images.unsplash.com/photo-1524222541586-9cceac1ba681?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=cc5ace4cf450baf7d68112e4e74b71d6&auto=format&fit=crop&w=800&q=60"
 ];
