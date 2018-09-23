a8c89ef870e1b9ffa92ca99bc17fa84d364b84b2
Carolina Pinzon S <carolina@axiomzen.co>
2018-09-22 18:16:41 -0700

    add NavHeaderController

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,39 +15,42 @@ import {
 } from './referralsFormFields';
 import httpClient from './httpClient';
 import customRoutes from './customRoutes';
+import NavHeaderController from './NavHeaderController';
 
 import './App.css';
 
 const dataProvider = postgrestProvider('/api', httpClient);
 const authProvider = createAuthClient('/api');
-const App = () => (
-  <Admin
-    dataProvider={dataProvider}
-    authProvider={authProvider}
-    customRoutes={customRoutes}
-  >
-    <Resource
-      options={{ label: 'Clients form' }}
-      name="referrals_form_inputs"
-      list={ReferralsFormFieldsList}
-      create={ReferralsFormFieldsCreate}
-      edit={ReferralsFormFieldsEdit}
-    />
-    <Resource
-      name="clients"
-      list={ClientList}
-      create={ClientCreate}
-      edit={ClientEdit}
-    />
-
-    <Resource
-      options={{ label: 'Shopping lists' }}
-      name="shopping_list_items"
-      list={ShoppingListsList}
-      create={ShoppingListsCreate}
-      edit={ShoppingListsEdit}
-    />
-  </Admin>
-);
+const App = () => {
+  return (
+    <Admin
+      dataProvider={dataProvider}
+      authProvider={authProvider}
+      customRoutes={customRoutes}
+    >
+      <NavHeaderController />
+      <Resource
+        options={{label: 'Clients form'}}
+        name="referrals_form_inputs"
+        list={ReferralsFormFieldsList}
+        create={ReferralsFormFieldsCreate}
+        edit={ReferralsFormFieldsEdit}
+      />
+      <Resource
+        name="clients"
+        list={ClientList}
+        create={ClientCreate}
+        edit={ClientEdit}
+      />
+      <Resource
+        options={{label: 'Shopping lists'}}
+        name="shopping_list_items"
+        list={ShoppingListsList}
+        create={ShoppingListsCreate}
+        edit={ShoppingListsEdit}
+      />
+    </Admin>
+  );
+};
 
 export default App;
